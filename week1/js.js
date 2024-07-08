a80f57a6850329be0f1ff6d514520b95c0ffd29b
salmanpoomonmlp <salman567633800@gmail.com>
2024-07-08 15:49:34 +0530

    first commit

--- week1/js.js
+++ week1/js.js
@@ -1,11 +1,11 @@
 console.log("start")
 
-function task1(){
+ function task1(){
   
     
     return new Promise((res,rej)=>{
     setTimeout(()=>{
-        const me = false
+        const me = true
         me==true ?  res("wake up"):rej("sleeping")
        
       
@@ -41,11 +41,55 @@ function task4(){
 })
 }
 
-task1().then((val)=>{console.log(val); return task2()})
-       .then((val)=>{console.log(val); return task3()})
-       .then((val)=>{console.log(val); return task4()})
-       .then((val)=>{console.log(val);return console.log("end")})
-       .catch(error => console.error(error))
+async function douit(){
+  try{
+    const wakep = await task1()
+    console.log(wakep);
+ 
+    const food = await task2()
+    console.log(food);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
 
+
+
+douit()
+
+
+
+// task1().then((val)=>{console.log(val); return task2()})
+//        .then((val)=>{console.log(val); return task3()})
+//        .then((val)=>{console.log(val); return task4()})
+//        .then((val)=>{console.log(val);return console.log("end")})
+//        .catch(error => console.error(error))
+
+
+
+
+
+
+
+
+
+// console.log("starting")
+// let x=fetch('"https://example.org/products.json"')
+
+// console.log(x);
+// x.then((data)=>[
+//     console.log(data)
+// ])
+
+// async function num(n){
+//     return n
+// }
+
+// x=num(12)
+// x.then((data)=>{
+//     console.log(data)
+// })
+// console.log(x);
 
 
