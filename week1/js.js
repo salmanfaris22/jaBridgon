b5f1ef4535507cde2f8d22f5304351f547b1b27f
salmanpoomonmlp <salman567633800@gmail.com>
2024-07-09 10:22:14 +0530

    first commit

--- week1/js.js
+++ week1/js.js
@@ -1,62 +1,62 @@
-console.log("start")
+// console.log("start")
 
- function task1(){
+//  function task1(){
   
     
-    return new Promise((res,rej)=>{
-    setTimeout(()=>{
-        const me = true
-        me==true ?  res("wake up"):rej("sleeping")
+//     return new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         const me = true
+//         me==true ?  res("wake up"):rej("sleeping")
        
       
-    },3000)
-    })
-}
-function task2(){
+//     },3000)
+//     })
+// }
+// function task2(){
 
-return new Promise((res,rej)=>{
-    setTimeout(()=>{
-        const me = true;
-      me==true ?   res("eat food"):rej("i cant eat")
+// return new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         const me = true;
+//       me==true ?   res("eat food"):rej("i cant eat")
     
-    },2000)
-})
-}
+//     },2000)
+// })
+// }
 
-function task3(){
-  return new Promise((res,rej)=>{
-    setTimeout(()=>{
-        res("fresh//")
+// function task3(){
+//   return new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         res("fresh//")
   
-    },1000)
-})
-}
+//     },1000)
+// })
+// }
 
-function task4(){
-  return new Promise((res,rej)=>{
-    setTimeout(()=>{
-        res("goine out ...")
+// function task4(){
+//   return new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         res("goine out ...")
        
-    },6000)
-})
-}
+//     },6000)
+// })
+// }
 
-async function douit(){
-  try{
-    const wakep = await task1()
-    console.log(wakep);
+// async function douit(){
+//   try{
+//     const wakep = await task1()
+//     console.log(wakep);
  
-    const food = await task2()
-    console.log(food);
-  }
-  catch(error){
-    console.log(error);
-  }
-}
+//     const food = await task2()
+//     console.log(food);
+//   }
+//   catch(error){
+//     console.log(error);
+//   }
+// }
 
 
 
-douit()
+// douit()
 
 
 
@@ -74,22 +74,77 @@ douit()
 
 
 
-// console.log("starting")
-// let x=fetch('"https://example.org/products.json"')
+// // console.log("starting")
+// // let x=fetch('"https://example.org/products.json"')
 
-// console.log(x);
-// x.then((data)=>[
-//     console.log(data)
-// ])
+// // console.log(x);
+// // x.then((data)=>[
+// //     console.log(data)
+// // ])
 
-// async function num(n){
-//     return n
+// // async function num(n){
+// //     return n
+// // }
+
+// // x=num(12)
+// // x.then((data)=>{
+// //     console.log(data)
+// // })
+// // console.log(x);
+
+
+
+
+
+
+//  async function abc(){
+ 
+//      return p
+ 
 // }
-
-// x=num(12)
-// x.then((data)=>{
-//     console.log(data)
+// var p = new Promise((res,rej)=>{
+//   res("hy")
 // })
-// console.log(x);
+// const data = abc()
+// data.then((res)=> console.log(res))
+
+
+
+
+ const  p1 = new Promise((res,rej)=>{
+  setTimeout((da)=>{
+    res("tsak one compleated")
+  },500)
+
+}) 
+
+
+
+async function abc(){
+  
+const p3 = new Promise((res,rej)=>{
+  setTimeout(()=>{
+    res("task two compleated")
+  },1000)
+
+
+})
+
+}
+
+
+const p3 = new Promise((res,rej)=>{
+  setTimeout(()=>{
+    res("task three compleated")
+  },5000)
+
+
+})
+
+
+
+
+
+
 
 
